8aee2231638dd44089715a624837f59dc79d629e
mikoto <mikoto@ungine.cn>
2022-06-03 23:10:30 +0800

    [feat]: 添加GetArtworks的Parm

--- src/main/java/net/mikoto/pixiv/api/http/database/artwork/GetArtworks.java
+++ src/main/java/net/mikoto/pixiv/api/http/database/artwork/GetArtworks.java
@@ -12,6 +12,11 @@ import javax.servlet.http.HttpServletResponse;
  */
 @HttpApiPackage("net.mikoto.pixiv.api.http.database")
 public interface GetArtworks {
+    String PARAM_CREDENTIAL = "credential";
+    String PARAM_ORDER = "order";
+    String PARAM_PROPERTIES = "properties";
+    String PARAM_PAGE_COUNT = "pageCount";
+
     /**
      * Get artworks.
      *
