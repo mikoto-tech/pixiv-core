4c301341cfebea5a245fd1f96a7722606e19e303
mikoto <mikoto@ungine.cn>
2022-06-04 20:04:15 +0800

    [feat] 调整ForwardServer模型

--- src/main/java/net/mikoto/pixiv/api/model/ForwardServer.java
+++ src/main/java/net/mikoto/pixiv/api/model/ForwardServer.java
@@ -7,28 +7,52 @@ import java.util.Objects;
  * @date 2022/3/27 3:42
  */
 public class ForwardServer {
-    private final String address;
-    private final int weight;
+    private String address;
+    private int weight;
     private int currentWeight = 0;
     private String key;
 
-    public ForwardServer(String address, int weight) {
-        this.address = address;
-        this.weight = weight;
+    @Override
+    public String toString() {
+        return "Mikoto-Pixiv-Generic-Data-Type[" + this.getClass().getPackage().getName() + this.getClass().getName() + "]:\nForwardServer{" +
+                "address='" + address + '\'' +
+                ", weight=" + weight +
+                ", currentWeight=" + currentWeight +
+                ", key='" + key + '\'' +
+                '}';
     }
 
-    public ForwardServer(String address, int weight, String key) {
-        this.address = address;
-        this.weight = weight;
-        this.key = key;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ForwardServer that = (ForwardServer) o;
+        return weight == that.weight && currentWeight == that.currentWeight && Objects.equals(address, that.address) && Objects.equals(key, that.key);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(address, weight, currentWeight, key);
     }
 
     public String getAddress() {
         return address;
     }
 
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
     public int getWeight() {
         return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
     }
 
     public int getCurrentWeight() {
@@ -45,31 +69,5 @@ public class ForwardServer {
 
     public void setKey(String key) {
         this.key = key;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof ForwardServer that)) {
-            return false;
-        }
-        return getWeight() == that.getWeight() && getCurrentWeight() == that.getCurrentWeight() && getAddress().equals(that.getAddress());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getAddress(), getWeight(), getCurrentWeight());
-    }
-
-    @Override
-    public String toString() {
-        return "Mikoto-Pixiv-Generic-Data-Type[" + this.getClass().getPackage().getName() + this.getClass().getName() + "]:\nForwardServer{" +
-                "address='" + address + '\'' +
-                ", weight=" + weight +
-                ", currentWeight=" + currentWeight +
-                ", key='" + key + '\'' +
-                '}';
     }
 }
